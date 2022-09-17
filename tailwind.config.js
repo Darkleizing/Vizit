df777b72a570ae8e7cbceadfe6dde284a138ad7f
Darkleizing <101994645+Darkleizing@users.noreply.github.com>
2022-09-17 11:12:19 +0300

    upd 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.html"],
+  content: [
+  './*.html',
+  './node_modules/flowbite/**/*.js'
+  ],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
   plugins: [
     require('flowbite/plugin')
-],
+  ],
 }
